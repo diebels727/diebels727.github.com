9cfc68c901e8428f1dabbfecf03249f1a31d46cc
Your Name <you@example.com>
2020-11-27 10:23:02 -0500

    x
    comment.
    
    x
    
    x

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.addEventListener('fetch', function(event) {
   console.log(`fetch event(${event}).`);
-  response = new Response(`<script>alert('hello from js.');</script>`,
-    headers: {'Content-Type': 'application/js', 'service-worker-allowed': '/', 'foo': 'bar'});
+  response = new Response(`<script>alert('hello from js.');</script>`, {headers: {'Content-Type': 'application/js', 'service-worker-allowed': '/sw/', 'foo': 'bar'}});
   console.log(`responding with response(${response}).`);
   event.respondWith(response);
 });
