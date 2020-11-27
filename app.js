720c55e03a644a7bd37b88787b01b37760d423d2
Your Name <you@example.com>
2020-11-27 10:09:02 -0500

    fix paren.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js', {scope: './'})
   .then((reg) => {
-    console.log(`registration success scope(${reg.scope}).');
+    console.log(`registration success scope(${reg.scope}).`);
   }).catch((error) => {
     console.log(`registration error(${error})`);
   });
