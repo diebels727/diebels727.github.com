093c55e8b36e7bb2700444da73600eea0d12d44d
Your Name <you@example.com>
2020-11-27 10:07:21 -0500

    add sw.

---
+++ app.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', {scope: './'})
+  .then((reg) => {
+    console.log(`registration success scope(${reg.scope}).');
+  }).catch((error) => {
+    console.log(`registration error(${error})`);
+  });
+}
