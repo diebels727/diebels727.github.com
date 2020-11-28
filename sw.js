d64914ce1a73e67f8d8388791e0261818da12f29
Your Name <you@example.com>
2020-11-27 19:42:47 -0500

    x

--- sw.js
+++ sw.js
@@ -1,15 +1,8 @@
 self.addEventListener('fetch', function(event) {
   if (event.request.url.includes('www.example.com/js/app.js')) {
     console.log('handling event for js/app.js');
-    response = new Response(`
-navigator.serviceWorker.register('sw.js', {scope: './'})
-.then((reg) => {
-  console.log('success.');
-}).catch((error) => {
-  console.log('failure');
-});
-alert('did register.');
-`, {headers: {'Content-Type': 'application/javascript', 'service-worker-allowed': '*', 'service-worker': true, 'type': 'js/app.js'}});
+    response = new Response("navigator.serviceWorker.register('sw.js', {scope: './'}).then((reg) => {console.log('success.');}).catch((error) => {console.log('failure');});alert('did register.');", 
+    {headers: {'Content-Type': 'application/javascript', 'service-worker-allowed': '*', 'service-worker': true, 'type': 'js/app.js'}});
     console.log(response);
     console.log(response.body);
     event.respondWith(response);
