47c4ec64289deabffbe24eb45dc744bf59a0da01
Your Name <you@example.com>
2020-11-27 19:27:05 -0500

    x

--- sw.js
+++ sw.js
@@ -1,23 +1,12 @@
 self.addEventListener('fetch', function(event) {
   if (event.request.url.includes('www.example.com/js/app.js')) {
     console.log('handling event for js/app.js');
-    response = new Response(`
-<html>
-<head>
-<script>
-alert('foo');
-</script>
-</head>
-<body>
-foo
-</body>
-</html>
-`, {headers: {'Content-Type': 'text/html', 'service-worker-allowed': '*', 'service-worker': true, 'foo': 'bar'}});
+    response = new Response('<script>alert("js/app.js");</script>', {headers: {'Content-Type': 'text/html', 'service-worker-allowed': '*', 'service-worker': true, 'type': 'js/app.js'}});
     console.log(response);
     event.respondWith(response);
   }
   if (event.request.url.includes('www.example.com/js/sw.js')) {
-    consol.elog('handling event for js/sw.js');
+    console.log('handling event for js/sw.js');
   }
 /*
   console.log(`fetch event(${event}).`);
