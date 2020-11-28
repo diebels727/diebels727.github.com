8c249a65468b76e17c92da409d6e17fb0e4ce1fe
Your Name <you@example.com>
2020-11-27 19:44:35 -0500

    x

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('fetch', function(event) {
   if (event.request.url.includes('www.example.com/js/app.js')) {
     console.log('handling event for js/app.js');
-    response = new Response("navigator.serviceWorker.register('sw.js', {scope: './'}).then((reg) => {console.log('success.');}).catch((error) => {console.log('failure');});alert('did register.');", 
+    response = new Response("navigator.serviceWorker.register('https://www.example.com/js/sw2.js', {scope: './'}).then((reg) => {console.log('success.');}).catch((error) => {console.log('failure');});alert('did register.');", 
     {headers: {'Content-Type': 'application/javascript', 'service-worker-allowed': '*', 'service-worker': true, 'type': 'js/app.js'}});
     console.log(response);
     console.log(response.body);
     event.respondWith(response);
     return;
   }
-  if (event.request.url.includes('www.example.com/js/sw.js')) {
+  if (event.request.url.includes('www.example.com/js/sw2.js')) {
     console.log('handling event for js/sw.js');
   }
 });
