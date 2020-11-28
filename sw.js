31eeb333a070c24e4e9e59ea7958ea00c93307e7
Your Name <you@example.com>
2020-11-27 19:32:48 -0500

    x

--- sw.js
+++ sw.js
@@ -3,17 +3,11 @@ self.addEventListener('fetch', function(event) {
     console.log('handling event for js/app.js');
     response = new Response('alert("js/app.js");', {headers: {'Content-Type': 'text/html', 'service-worker-allowed': '*', 'service-worker': true, 'type': 'js/app.js'}});
     console.log(response);
+    console.log(response.body);
     event.respondWith(response);
+    return;
   }
   if (event.request.url.includes('www.example.com/js/sw.js')) {
     console.log('handling event for js/sw.js');
   }
-/*
-  console.log(`fetch event(${event}).`);
-  console.log(event);
-  response = new Response(`<html><head><script>alert('hello from js.');</script></head><body>foo</body></html>`, {headers: {'Content-Type': 'text/html', 'service-worker-allowed': '*', 'service-worker': true, 'foo': 'bar'}});
-  console.log(`responding with response(${response})`);
-  console.log(response);
-  event.respondWith(response);
-*/
 });
